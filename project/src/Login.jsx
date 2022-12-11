5761d73675fd52d1fb3f87eb35b853aec9697603
Hannah <hannah.yuen719@gmail.com>
2022-12-11 19:51:33 +0800

    WIP:Login

--- project/src/Login.jsx
+++ project/src/Login.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Navigate } from "react-router-dom";
 
-function Login() {
-    return (
-        <div>
-            <a href="/User"> Login </a>
+function Login(){
+    let [role,setRole] = useState('');
+
+    var objPeople = [
+        {
+            username: "Hannah",
+            password: "Hello",
+            role: "user"
+        },
+        {
+            username: "Admin",
+            password: "Admin",
+            role: "Admin"
+        }
+    ]
+
+   let logInPage = e =>{
+        e.preventDefault();
+        let username = e.target.username.value;
+        let password = e.target.password.value;
+        if(username == "Hannah" && password == "Hello")  setRole('user')
+   }
+    return(
+        <div id='logInPage'>
+            <header>
+                <h5>Log in page- CSCI2720 Project</h5>
+            </header>
+            <main>
+                {
+                    Login=="false" && 
+                    <div>Login Failure. Plz ensure username and account correct.</div>
+                }
+                <div className='loginBox'>
+                    <form onSubmit={logInPage}>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name="username"></input><br />
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name="password"></input><br />
+                        <input className='loginbtn' type="submit" value="Login" />
+                    </form>
+                </div>
+            </main>
         </div>
-    );
+    )
 }
 
 export default Login;
