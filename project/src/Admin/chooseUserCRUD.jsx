c6fe33c478a616176ad525dd57ff3398c43ac836
VioletChan04 <75989133+Wei04@users.noreply.github.com>
2022-12-12 18:28:30 +0800

    choose from normal users or admin CRUD

---
+++ project/src/Admin/chooseUserCRUD.jsx
@@ -0,0 +1,6 @@
+// Au Yeung Hui Kei (1155158820) Chan Gi Wei Violet (1155126648) 
+// Kong Ming Kin(1155144268)     Lo Kwun Hei (1155155612) 
+// Yeung Ka Him(1155176590)      Yuen Man Yi (1155127553)
+
+
+//choose from normal users or admin CRUD
