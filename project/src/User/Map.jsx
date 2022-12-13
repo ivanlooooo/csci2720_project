a54208455400ad2eb2fc046272cf34c4686eb57b
Hannah <hannah.yuen719@gmail.com>
2022-12-13 23:04:24 +0800

    Map

--- project/src/User/Map.jsx
+++ project/src/User/Map.jsx
@@ -1,54 +1,96 @@
-import React from "react";
+import React, { Component, useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-function SimpleMap() {
-    const defaultProps = {
-        center: {
-            lat: 10.99835602,
-            lng: 77.01502627
+
+function GoogleMap(props) {  
+
+    const [mapApiLoaded, setMapApiLoaded] = useState(false)
+    const [mapInstance, setMapInstance] = useState(null)
+    const [mapApi, setMapApi] = useState(null)
+
+    const [myPosition, setMyPosition] = useState({
+        lat: 25.04,
+        lng: 121.50
+      })
+
+    let location=[
+        {
+            placeId : 3110031,
+            lat: 22.501639,
+            lng: 114.128911,
+            text: 'North District Town Hall (Auditorium)'
         },
-        zoom: 11
-    };
+        {
+            placeId : 36310035,
+            lat: 22.38136,
+            lng: 114.18990,
+            text: 'Sha Tin Town Hall (Auditorium)'
+        },
+        {
+            placeId : 75010017,
+            lat: 22.285056,
+            lng: 114.222075,
+            text: 'Hong Kong Film Archive (Cinema)'
+        },
+    ]
 
-    return (
-        // Important! Always set the container height explicitly
-        <div style={{ height: '100vh', width: '100%' }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API_KEY }}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
-            >
-                <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
-                    text="My Marker"
-                />
-            </GoogleMapReact>
+    const apiHasLoaded = (map, maps) => {
+        setMapInstance(map)
+        setMapApi(maps)
+        setMapApiLoaded(true)
+      };
+    
+    const handleCenterChange = () => {
+        if(mapApiLoaded) {
+          setMyPosition({
+            lat: mapInstance.center.lat(),
+            lng: mapInstance.center.lng()
+          })
+        }
+    }
+
+    const MeseumMarker = ({ icon, text }) => (
+        <div>
+          <img style={{ height: '30px', width: '30px' }} src={icon} />
+          <div>{text}</div>
         </div>
-    );
-}
+    )
 
-function GoogleMap() {
     return (
         <section id="map">
-            <div className='container-fluid'>
-                <div className='row'>
-                    <div className='col'>
-                        <h1>Google Map</h1>
-                    </div>
-                </div>
-
-                <div className='row'>
-                    <div className='col'>
-                        <p>this is the location for google map, uncomment code when you have enabled api key, then insert the api key into .env file</p>
-                        <SimpleMap /> 
-                    </div>
-                </div>
+            <input type="button" value="測試自動完成" onClick={handleAutocomplete} />
+            <div style={{ height: '100vh', width: '70%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API_KEY }}
+                    onBoundsChange={handleCenterChange}
+                    defaultCenter={props.center}
+                    defaultZoom={15}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({ map, maps }) => apiHasLoaded(map, maps)}
+                >
+                {location.map(item=>(
+                        <MeseumMarker
+                            icon={require('../image/museum.png')}
+                            lat={item.lat}
+                            lng={item.lng}
+                            text={item.text}
+                            placeId={item.placeId}
+                        />
+                ))}
+                
+                </GoogleMapReact>
             </div>
         </section>
     )
 }
+GoogleMap.defaultProps = {
+    center: {
+      lat: 22.4185 ,
+      lng: 114.2041
+    },
+    zoom: 17
+  };
 
 export default GoogleMap;
