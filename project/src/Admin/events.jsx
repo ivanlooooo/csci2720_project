06efd7781eab1be876c74ef2885818443982ea77
VioletChan04 <75989133+Wei04@users.noreply.github.com>
2022-12-14 00:07:08 +0800

    show events

--- project/src/Admin/events.jsx
+++ project/src/Admin/events.jsx
@@ -2,4 +2,118 @@
 // Kong Ming Kin(1155144268)     Lo Kwun Hei (1155155612) 
 // Yeung Ka Him(1155176590)      Yuen Man Yi (1155127553)
 
+//show the events
 
+
+import { useNavigate, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+//import '../css/admin/location.css';
+
+//only for admin
+function AdminOnly(){
+    return <h3>Admin only, please log in as admin.</h3>
+}
+
+function Events(){
+    let navigate = useNavigate();
+    let [role, setRole] = useState(null)
+    let [locations, setLocations] = useState(null)
+
+    let crudLoc = param => navigate("crud?"+param);
+    let reloadData = () => {
+        fetch(process.env.REACT_APP_SERVER_URL+"/locationManage",{
+            method:"POST",
+            credentials: "include",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ option: "reload" })
+        })
+        .then(res => res.json())
+        .then(res => setLocations(res))
+        .catch(err => console.log("error: "+err));
+    }
+
+    useEffect(()=>{
+        fetch(process.env.REACT_APP_SERVER_URL+"/checkRole",{
+            method:"POST",
+            credentials: "include"
+        })
+        .then(res => res.json())
+        .then(res => setRole(res.role))
+        .catch(err => console.log("error: "+err));
+        
+        fetch(process.env.REACT_APP_SERVER_URL+"/locationManage",{
+            method:"POST",
+            credentials: "include",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ option: "readAll" })
+        })
+        .then(res => res.json())
+        .then(res => setLocations(res))
+        .catch(err => console.log("error: "+err));
+    },[]) 
+
+    return(
+        <>
+            { role === "error" && <Navigate to="/login" /> }
+            { role === null && <LoadingContent /> }
+            { role === "admin" && <LocationsContent locations={locations} reloadData={reloadData} crudLoc={crudLoc} /> }
+            { (role !== "admin" && role !== null) && <WrongRole />}    
+        </>   
+    )
+}
+
+function LoadingContent(){
+    let [dotNum, setDotNum] = useState(0)
+    useEffect(()=>{
+        let timer = setTimeout(()=>setDotNum((dotNum+1)%4),1000)
+        return () => clearTimeout(timer)
+    }) 
+    return <h1>{".".repeat(dotNum)}</h1>
+}
+
+
+function LocationsContent(props){
+
+    return(
+        <section id='locationManage'>
+            <div className='container-xl'>
+                <div className='row'>
+                    <div className='col'>
+                        <h1>Event List</h1>
+                    </div>
+                    <div className="col fnt-nav">
+                        <button type="button" className="nav-btn fnt-btn" onClick={() => props.crudLoc("option=create")}>Create New Location</button>
+                        <button className="nav-btn refresh-btn" onClick={props.reloadData}>Refresh</button>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <table className="location-table">
+                            <thead>
+                                <tr className="first-row">
+                                    <th>Place</th>
+                                    <th>Event</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {props.locations!=null && props.locations.map((ele,i) => 
+                                    <tr key={i}>
+                                        <td>{ele.name}</td>
+                                        <td>{ele.events}</td>
+                                        <td>
+                                            <button type="button" className="button" onClick={() => props.crudLoc("option=read&id="+ele._id)}>Read</button>
+                                            <button type="button" className="button update-btn" onClick={() => props.crudLoc("option=update&id="+ele._id)}>Update</button>
+                                            <button type="button" className="button delete-btn" onClick={() => props.crudLoc("option=delete&id="+ele._id)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                )} 
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Events;
