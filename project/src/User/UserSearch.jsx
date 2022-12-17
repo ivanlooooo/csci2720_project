c11223624b26d73c771105a0468dbc9aba1d9e2a
Hannah <hannah.yuen719@gmail.com>
2022-12-17 12:34:11 +0800

    update Search

--- project/src/User/UserSearch.jsx
+++ project/src/User/UserSearch.jsx
@@ -1,12 +1,12 @@
 import { useNavigate, Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 
 import UserPanel from './UserPanel';
 import '../CSS/User/UserSearch.css';
-
 function UserSearch() {
     let navigate = useNavigate();
     let [locations, setLocations] = useState(null)
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
 
     let visitLoc = id => navigate("../location?id="+id);
 
@@ -19,14 +19,44 @@ function UserSearch() {
           }).then(response=>response.json())
           .then(res => {
             res.error? console.log(res.error):setLocations(res.locList)
+            locations = res.locList;
             console.log(res)
             return(res)
         })
         .then(res=>{
-            console.log(locations)}
+            //console.log(locations)
+        }
             )
         .catch(err => console.log("error: "+err));
-    },[]) 
+    },[]);
+ 
+    useEffect(() => {
+        fetch(process.env.REACT_APP_SERVER_URL + "/getallevent", {
+            method: "POST",
+            credentials: "include",
+            headers: { 'Content-Type': 'application/json' }
+        }).then(response => response.json())
+            .then(res => {
+                //console.log(res);
+                locations.forEach(element => {
+                    element.count = 0;
+                })
+                res.forEach(element => {
+                    locations.forEach(element2 => {
+                        if (element.venue[0] == element2.name) {
+                            if (element2.count) {
+                                element2.count ++;
+                            } else {
+                                element2.count = 1;
+                            }
+                        }
+                    })
+                });
+                console.log(locations);
+                setLocations(locations);
+                forceUpdate();
+            })
+    }, []);
 
     const [searchInput, setSearchInput] = useState("");
 
@@ -68,7 +98,7 @@ function UserSearch() {
                                 <td className="Cell" id="Lat">{key.longitude}</td>
                                 <td className="Cell" id="Lon">{key.latitude}</td>
                                 <td className="Cell" id="Loc ID">{key.locId}</td>
-                                <td className="Cell" id="eventCount">Later add back</td>
+                                <td className="Cell" id="eventCount">{key.count}</td>
                                 <td><button type="button" onClick={() => visitLoc(key.locId)}>Visit</button></td>
                                 <td><button type="button">Add Fav</button></td>
                             </tr>
